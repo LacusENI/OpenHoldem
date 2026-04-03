4e5cae172fb80d7734bd15f3dee6d70cb3d39671
Lacus <2854651899@qq.com>
2026-04-03 17:07:56 +0800

    feat(main): 添加可执行程序

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+#include "Card.h"
+
 int main() {
+    std::cout << holdem::Card("CA").toMessage();
+    std::cout << holdem::Card("DJ").toMessage();
+    std::cout << holdem::Card("HT").toMessage();
+    std::cout << holdem::Card("S8").toMessage();
+    std::cout << "\n";
+    std::cout << "OpenHoldem\n";
     std::cout << "Hello World!\n";
     return 0;
 }
