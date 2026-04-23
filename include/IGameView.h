ffa81271e452ca923e1e0f283fca92c14a51e452
Lacus <2854651899@qq.com>
2026-04-23 20:32:56 +0800

    refactor(view): 添加传给 View 的参数对象

--- include/IGameView.h
+++ include/IGameView.h
@@ -5,6 +5,45 @@
 #include "GameModel.h"
 
 namespace holdem {
+
+struct OnGameStartedData {
+    Position btn_position;
+};
+
+struct OnRoundStartedData {
+    const GameModel& game_model;
+};
+
+struct OnPlayerTurnData {
+    const Position position;
+};
+
+struct PlayerInputData {
+    Action action;
+};
+
+struct OnPlayerActedData {
+    const Action& action;
+};
+
+struct OnRoundEndedData {
+
+};
+
+struct OnShowdownCompletedData {
+    const GameModel& game_model;
+    const std::vector<std::pair<Position, HandValue>>& results;
+};
+
+struct OnWinnerDeclaredData {
+    const std::vector<Position>& winners;
+    const std::vector<Stack>& amounts;
+};
+
+struct OnGameOverData {
+    const GameModel& game_model;
+};
+
 class IGameView {
 public:
     virtual ~IGameView() = default;
